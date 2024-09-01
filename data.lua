5c290a856ceff71af94455f33e123defea4db30b
Artik-Star1 <artikstar7076@gmail.com>
2024-09-02 00:22:23 +0500

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,7 +1,1 @@
-return {
-    script_expiry_date = "2024-12-31",  -- Дата окончания действия скрипта
-    users = {
-        { login = "blesk", password = "arman", expiry_date = "2025-01-01" },
-        { login = "userr", password = "passwordd", expiry_date = "2024-08-31" },
-    }
-}
+expiry_date = "2025-08-18"
