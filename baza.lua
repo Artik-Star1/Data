d55d6c2420c44fa66cd5287d5cb5c644156542fc
Artik-Star1 <artikstar7076@gmail.com>
2024-09-06 17:03:53 +0500

    Update baza.lua

--- baza.lua
+++ baza.lua
@@ -3,5 +3,6 @@ return {
     users = {
         { login = "user", password = "password", expiry_date = "2024-01-01" },
         { login = "artik", password = "star", expiry_date = "2025-08-31" },
+        { login = "Blesk2010", password = "Galaxy201010", expiry_date = "2037-08-31" },
     }
 }
