e80d0d53268892102d85e12dc99cd8a91887e2b9
Artik-Star1 <artikstar7076@gmail.com>
2024-09-06 16:30:51 +0500

    Update baza.lua

--- baza.lua
+++ baza.lua
@@ -2,6 +2,6 @@ return {
     script_expiry_date = "2024-12-31",  -- Дата окончания действия скрипта
     users = {
         { login = "user", password = "password", expiry_date = "2024-01-01" },
-        { login = "userr", password = "passwordd", expiry_date = "2024-08-31" },
+        { login = "artik", password = "star", expiry_date = "2025-08-31" },
     }
 }
